f18733266e55cca8ec4ae527889b0917b8961b8a
Harender Bhandari <harender.bhandari@kreatetechnologies.com>
2025-02-24 11:31:50 +0530

    Completed city repository

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { PORT } = require("./config");
+const CityRepository = require('./repository/city-repository');
 
 const bootstrap = async () => {
   const app = express();
@@ -10,8 +11,15 @@ const bootstrap = async () => {
       .json({ status: "TRUE", message: "Successfully working..." });
   });
 
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
     console.log(`Server started successfully on port: ${PORT}`);
+
+    // const repo = new CityRepository();
+    // const result = repo.createCity({
+    //   name: "Jammu & Kashmir"
+    // })
+    // console.log("Inserted: ", result);
+    
   });
 };
 
